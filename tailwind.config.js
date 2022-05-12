6e0819f680d599220e3c2f45bf37e3fccbd43568
pochin <qwe852147@hotmail.com>
2022-05-12 19:42:18 +0800

    for example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   // prefix: 'bo-',
   // important: true,
   // separator:'_',
   theme: {
+    alternative: {
+      one: "1",
+      two: "2",
+      three: "3",
+      ten: "10",
+    },
     opacity: {
       10: "0.1",
       20: "0.2",
@@ -29,4 +37,50 @@ module.exports = {
   //   backgroundColor: false,
   //   textColor: false,
   // },
+  plugins: [
+    plugin(function ({
+      addUtilities,
+      matchUtilities,
+      addComponents,
+      matchComponents,
+      addBase,
+      theme,
+    }) {
+      addBase({
+        // add Default
+        h1: { fontSize: theme("fontSize.2xl") },
+        // h2: { fontSize: theme("fontSize.xl") },
+        // h3: { fontSize: theme("fontSize.lg") },
+      });
+
+      addUtilities({
+        ".content-red": {
+          color: "red",
+        },
+        ".content-blue": {
+          color: "blue",
+        },
+      });
+
+      matchUtilities(
+        {
+          matchborderU: (value) => ({
+            borderWidth: value + "px",
+            borderColor: "blue",
+          }),
+        },
+        { values: theme("alternative") }
+      );
+
+      matchComponents(
+        {
+          matchborderC: (value) => ({
+            borderWidth: value + "px",
+            borderColor: "blue",
+          }),
+        },
+        { values: theme("alternative") }
+      );
+    }),
+  ],
 };
